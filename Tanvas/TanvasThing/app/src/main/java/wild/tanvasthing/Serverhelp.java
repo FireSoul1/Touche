0d894ce8845ee401eed30356ac655958dc027be3
FireSoul1 <dragonball1>
2016-11-19 11:07:19 -0600

    Client is started and can open up things

---
+++ Tanvas/TanvasThing/app/src/main/java/wild/tanvasthing/Serverhelp.java
@@ -0,0 +1,64 @@
+package wild.tanvasthing;
+
+import android.os.Environment;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+/**
+ * Created by GbearTheGenius on 11/19/16.
+ */
+
+public class Serverhelp {
+    private String host;
+    private int port;
+
+    Serverhelp(int port, String host) {
+        this.port = port;
+        this.host = host;
+    }
+    public int uploadImage(String path) {
+
+        Socket sock = new Socket();
+        try {
+            Log.d("Sockets","Opening the socket --> ");
+            sock.connect((new InetSocketAddress(host, port)));
+            final File f = new File(
+                    Environment.getExternalStorageDirectory()
+                            + "/wifip2pshared-"
+                            + System.currentTimeMillis() + ".jpg");
+            File dirs  = new File(f.getParent());
+
+            if (!dirs.exists())
+                dirs.mkdirs();
+            f.createNewFile();
+
+            // send Data To Server
+            OutputStream stream = sock.getOutputStream();
+            FileInputStream file = new FileInputStream(
+                    "/sdcard/samsung/Image/001" + ".jpg");
+
+
+            file.close();
+
+        } catch (Exception e) {
+
+        }
+
+        //the process worked
+        return 42;
+    }
+    public int getnewImage() {
+
+
+        //the process worked
+        return 42;
+
+    }
+
+}
